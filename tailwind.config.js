7110f1d6ce184d5cc867d786a292002fa31bd5f4
Tom Worrall <hello@tcfw.com.au>
2021-06-25 15:46:59 +1000

    set default font to something awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
@@ -5,6 +7,9 @@ module.exports = {
     extend: {
       colors: {
         'ataas-gray': '#393E44',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
